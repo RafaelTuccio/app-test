10c3d5e63fca81f497cebc03b760b918f6db08f2
RafaelTuccio <rafael_tuccio@hotmail.com>
2021-06-04 20:13:42 -0300

    teste novo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Pagina inicial testando commits</h1>
+     <h1>Pagina inicial testando commits 2</h1>
     </div>
   );
 }
